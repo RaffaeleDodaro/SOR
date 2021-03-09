2efbaa09c86f660561cc7d447f3730e88fbed862
Raffaele Dodaro <68231732+RaffaeleDodaro@users.noreply.github.com>
2021-03-09 22:59:42 +0100

    l

--- Thread/SvolgimentoEsami/Giugno2018.py
+++ Thread/SvolgimentoEsami/Giugno2018.py
@@ -1,5 +1,5 @@
 import random, time, os
-from threading import Lock, Thread,Condition,RLock
+from threading import Thread,Condition,RLock
 from queue import Queue
 
 class DischiConcentrici:
@@ -12,19 +12,36 @@ class DischiConcentrici:
             self.out[i]=None
         self.lock=RLock()
         self.condition=Condition(self.lock)
+        self.shiftAttuale=0
+
+    def _om(self,i):
+        with self.lock:
+            return (i + self.shiftAttuale) % self.size
 
     def shift(self, m:int):
         with self.lock:
-            self.appoggio=[None * self.n]
+            self.shiftAttuale+=m
 
-    def shift(self,i:int,v:int,d:int):
-        if d==1:
-            pass
-        elif(d==0):
-            pass
+    def set(self,i:int,v:int,d:int):
+        with self.lock:
+            if d==1:
+                self.out[self._om(i)]=v
+            elif(d==0):
+                self.out[i]=v
+            if self.in[i]==self.out[self._om(i)]:
+                self.in[i]=0
+                self.out[self._om(i)]=0
+            elif v!=0:
+                self.condition.notifyAll()
+
 
     def get(self,i:int,d=int):
-        if d==1:
-            pass
-        elif(d==0):
-            pass
+        with self.lock:
+            while (d==1 and self.in[i]==0) or (d==0 and self.out[self._om(i)==0]):
+                    self.condition.wait()
+            
+            if d==1:
+                return self.in[i]
+                
+            elif(d==0):
+                return self.out[self._om(i)]
