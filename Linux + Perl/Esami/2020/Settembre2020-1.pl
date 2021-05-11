f34c68a12c1e0b669e44c506026441be81fea20f
Raffaele Dodaro <68231732+RaffaeleDodaro@users.noreply.github.com>
2021-05-11 22:35:42 +0200

    corrected 1 exercise

--- Linux + Perl/Esami/2020/Settembre2020-1.pl
+++ Linux + Perl/Esami/2020/Settembre2020-1.pl
@@ -2,24 +2,21 @@
 die $! if($#ARGV<0 or $#ARGV>1);
 $risorsa = shift or die $!;
 die $! if($risorsa ne "-c" or $risorsa ne "-m");
-$utente=shift or die $!;
+$utente = shift or die $!;
 %hash;
-foreach(qx{top -n1 -b})
-{
-    if($risorsa eq "-c")
-    {
-        $hash{$1}+=$2 if(m/\d+\s(\S+).+\w\s+(\d+\.\d)\s+(\d+\.\d)\s/);
-    }
-    else
-    {
-        $hash{$1}+=$3 if(m/\d+\s(\S+).+\w\s+(\d+\.\d)\s+(\d+\.\d)\s/);        
+foreach (qx{top -n1 -b}) {
+    if (m/\d+\s+(\S+)\s+\d+\s+\d+\s+\d+\s+\d+\s+\w\s+(\d+,\d+)\s+(\d+,\d+)/) {
+        $hash{$1} += $2 if ($risorsa =~ m/-c/);
+        $hash{$1} += $3 if ($risorsa =~ m/-m/);
     }
 }
-open($fh,">","stat.log") or die $!;
-print $fh "$utente --> $hash{$utente}\n";
-foreach(sort{$hash{$b}<=>$hash{$a}} keys %hash)
-{
-    print $fh "$_ --> $hash{$_}\n";
+open($fh, ">", "stat.log") or die $!;
+print $fh "utente $utente CPU: $hash{$utente}\n" if ($risorsa =~ m/-c/);
+print $fh "utente $utente MEM: $hash{$utente}\n" if ($risorsa =~ m/-m/);
+foreach (sort {$hash{$b} <=> $hash{$a}} keys %hash) {
+    print $fh "Max uso CPU: $utente $hash{$utente}%\n" if ($risorsa =~ m/-c/);
+    print $fh "Max uso MEM: $utente $hash{$utente}%\n" if ($risorsa =~ m/-m/);
+    print $fh "$hash{$_}\n";
     last;
 }
-close $fh or die $!;
+close $fh;
